439ae7682fe4a46dd64c47e9556c5e4b1d5fdabf
Apostolos Tzinas <tzinas@tzinas.com>
2024-02-17 15:46:30 +0200

    create coin, message, and stake transtaction

--- src/wallet.rs
+++ src/wallet.rs
@@ -3,10 +3,10 @@ use serde::{Deserialize, Serialize};
 use crate::crypto::{self, PrivateKey, PublicKey, Signed};
 
 pub struct Wallet {
-    /// The public key of this wallet.
-    pub public_key: PublicKey,
     /// The private key of this wallet.
     private_key: PrivateKey,
+    /// The public key of this wallet.
+    pub public_key: PublicKey,
     /// An auto-increment nonce used to sign transactions.
     nonce: u64,
 }
@@ -15,8 +15,6 @@ pub struct Wallet {
 pub struct Transaction {
     /// The public key of the sending wallet.
     pub sender_address: PublicKey,
-    /// The public key of the receiving wallet.
-    pub receiver_address: PublicKey,
     /// The kind of this transaction.
     pub kind: TransactionKind,
     /// The sender nonce.
@@ -27,47 +25,61 @@ impl Wallet {
     pub fn new() -> Self {
         let (private_key, public_key) = crypto::generate_keypair();
         Self {
-            public_key,
             private_key,
+            public_key,
             nonce: 0,
         }
     }
 
-    pub fn sign_coin_transaction(
-        &mut self,
-        receiver: &PublicKey,
-        amount: u64,
-    ) -> Signed<Transaction> {
+    fn create_transaction(&mut self,
+        kind: TransactionKind
+    ) -> Transaction {
         let nonce = self.nonce;
         self.nonce += 1;
 
-        let tx = Transaction {
+        Transaction {
             sender_address: self.public_key.clone(),
-            receiver_address: receiver.clone(),
-            kind: TransactionKind::Coin(amount),
-            nonce,
-        };
+            kind,
+            nonce
+        }
+    }
+
+    pub fn create_coin_transaction(
+        &mut self,
+        receiver: PublicKey,
+        amount: u64,
+    ) -> Signed<Transaction> {
+        let tx = self.create_transaction(TransactionKind::Coin(amount, receiver));
 
         self.private_key.sign(tx)
     }
 
-    pub fn create_message_transaction(&self /* args */) -> Transaction {
-        todo!()
+    pub fn create_message_transaction(
+        &mut self,
+        receiver: PublicKey,
+        message: String,
+    ) -> Signed<Transaction> {
+        let tx = self.create_transaction(TransactionKind::Message(message, receiver));
+
+        self.private_key.sign(tx)
     }
 
-    pub fn sign_transaction(&self, _tx: Transaction) -> Signed<Transaction> {
-        // 1. Calculate the hash of the transaction
-        // 2. Sign it using the provided key
-        todo!()
+    pub fn create_stake_transaction(
+        &mut self,
+        amount: u64,
+    ) -> Signed<Transaction> {
+        let tx = self.create_transaction(TransactionKind::Stake(amount));
+
+        self.private_key.sign(tx)
     }
 }
 
 #[derive(Debug, Clone, Eq, PartialEq, Hash, Serialize, Deserialize)]
 pub enum TransactionKind {
-    /// A coin transaction transferring the specified amount.
-    Coin(u64),
-    /// A message transaction transferring the specified message.
-    Message(String),
+    /// A coin transaction transferring the specified amount to the receiver.
+    Coin(u64, PublicKey),
+    /// A message transaction transferring the specified message to the receiver.
+    Message(String, PublicKey),
     // A staking transaction locking up the specified amount.
     Stake(u64),
 }
